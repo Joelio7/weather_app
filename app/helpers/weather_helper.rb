11a2a2c7e2cef740a56f81a0e06fe617fbf51be7
Andy Uptain <andylovesprograms@gmail.com>
2017-02-24 15:06:56 -0800

    Completed table so that the days correspond correctly beggining with the current day as the start of the week, cleaned up numerical data such as time and temperatures so that they look more presentable, the faren symbol has been moved outside the field of view until it can be implemented correctly so that it does not float inside the main temperature when the temperature is above 99 degrees

--- app/helpers/weather_helper.rb
+++ app/helpers/weather_helper.rb
@@ -13,7 +13,7 @@ module WeatherHelper
     weather_data << @current["current"]["condition"]["text"]
     weather_data << @current["current"]["condition"]["icon"]
 
-    weather_data
+    weather_data << @current["current"]["wind_dir"]
   end
 
   def city_name
@@ -41,14 +41,114 @@ module WeatherHelper
      parse_data[7]
   end
   def temp_f
-     parse_data[8]
+     parse_data[8].round / 1
   end
+
   def conditions
      parse_data[9]
   end
 
   def day_time?
      parse_data[10].include?("day") ? true : false
+  end
+
+  def wind_dir
+     parse_data[11]
+  end
+
+  def day3
+    t = Time.now
+    if t.sunday? == true
+      "Tuesday"
+    elsif t.monday? == true
+      "Wednesday"
+    elsif t.tuesday? == true
+      "Thursday"
+    elsif t.wednesday? == true
+      "Friday"
+    elsif t.thursday? == true
+      "Saturday"
+    elsif t.friday? == true
+      "Sunday"
+    elsif t.saturday? == true
+      "Monday"
+    end
+  end
+
+  def day4
+    t = Time.now
+    if t.sunday? == true
+      "Monday"
+    elsif t.monday? == true
+      "Tuesday"
+    elsif t.tuesday? == true
+      "Wednesday"
+    elsif t.wednesday? == true
+      "Thursday"
+    elsif t.thursday? == true
+      "Friday"
+    elsif t.friday? == true
+      "Saturday"
+    elsif t.saturday? == true
+      "Sunday"
+    end
+  end
+
+  def day5
+    t = Time.now
+    if t.sunday? == true
+      "Sunday"
+    elsif t.monday? == true
+      "Monday"
+    elsif t.tuesday? == true
+      "Tuesday"
+    elsif t.wednesday? == true
+      "Wednesday"
+    elsif t.thursday? == true
+      "Thursday"
+    elsif t.friday? == true
+      "Friday"
+    elsif t.saturday? == true
+      "Saturday"
+    end
+  end
+
+  def day6
+    t = Time.now
+    if t.sunday? == true
+      "Saturday"
+    elsif t.monday? == true
+      "Sunday"
+    elsif t.tuesday? == true
+      "Monday"
+    elsif t.wednesday? == true
+      "Tuesday"
+    elsif t.thursday? == true
+      "Wednesday"
+    elsif t.friday? == true
+      "Thursday"
+    elsif t.saturday? == true
+      "Friday"
+    end
+  end
+
+  def day7
+    t = Time.now
+    if t.sunday? == true
+      "Friday"
+    elsif t.monday? == true
+      "Saturday"
+    elsif t.tuesday? == true
+      "Sunday"
+    elsif t.wednesday? == true
+      "Monday"
+    elsif t.thursday? == true
+      "Tuesday"
+    elsif t.friday? == true
+      "Wednesday"
+    elsif t.saturday? == true
+      "Thursday"
+    end
   end
 
   def forecast_data
@@ -63,7 +163,8 @@ module WeatherHelper
    forecast_info
   end
   def current_time
-    Time.now
+    t = Time.now
+    t.strftime("%A %l:%M %p")
   end
 
   def day_1_date
@@ -71,11 +172,11 @@ module WeatherHelper
   end
 
   def day_1_maxtemp
-   forecast_data[1]
+   forecast_data[1].round / 1
   end
 
   def day_1_mintemp
-    forecast_data[2]
+    forecast_data[2].round / 1
   end
 
   def day_1_condition
@@ -87,11 +188,11 @@ module WeatherHelper
   end
 
   def day_2_maxtemp
-   forecast_data[5]
+   forecast_data[5].round / 1
   end
 
   def day_2_mintemp
-    forecast_data[6]
+    forecast_data[6].round / 1
   end
 
   def day_2_condition
@@ -103,11 +204,11 @@ module WeatherHelper
   end
 
   def day_3_maxtemp
-   forecast_data[9]
+   forecast_data[9].round / 1
   end
 
   def day_3_mintemp
-    forecast_data[10]
+    forecast_data[10].round / 1
   end
 
   def day_3_condition
@@ -119,11 +220,11 @@ module WeatherHelper
   end
 
   def day_4_maxtemp
-   forecast_data[13]
+   forecast_data[13].round / 1
   end
 
   def day_4_mintemp
-    forecast_data[14]
+    forecast_data[14].round / 1
   end
 
   def day_4_condition
@@ -135,11 +236,11 @@ module WeatherHelper
   end
 
   def day_5_maxtemp
-   forecast_data[17]
+   forecast_data[17].round / 1
   end
 
   def day_5_mintemp
-    forecast_data[18]
+    forecast_data[18].round / 1
   end
 
   def day_5_condition
@@ -150,11 +251,11 @@ module WeatherHelper
    forecast_data[20]
   end
   def day_6_maxtemp
-   forecast_data[21]
+   forecast_data[21].round / 1
   end
 
   def day_6_mintemp
-    forecast_data[22]
+    forecast_data[22].round / 1
   end
 
   def day_6_condition
@@ -165,11 +266,11 @@ module WeatherHelper
    forecast_data[24]
   end
   def day_7_maxtemp
-   forecast_data[25]
+   forecast_data[25].round / 1
   end
 
   def day_7_mintemp
-    forecast_data[26]
+    forecast_data[26].round / 1
   end
 
   def day_7_condition
