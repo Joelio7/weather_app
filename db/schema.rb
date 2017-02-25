7f0201aa0d2363b2bb5501a376ade4c9bb5cb77e
Your Name <joe.scalera@gmail.com>
2017-02-24 16:27:40 -0800

    removed sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -13,23 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20170203001123) do
 
-  create_table "us_cities", id: false, force: :cascade do |t|
-    t.integer "id",                                                 null: false
-    t.string  "name",          limit: 35
-    t.string  "county",        limit: 72
-    t.string  "state_code",    limit: 2
-    t.string  "state",         limit: 20
-    t.string  "zip_codes",     limit: 1685
-    t.string  "type",          limit: 23
-    t.decimal "latitude",                   precision: 8, scale: 5
-    t.decimal "longitude",                  precision: 8, scale: 5
-    t.string  "area_code",     limit: 3
-    t.integer "population"
-    t.integer "households"
-    t.integer "median_income"
-    t.integer "land_area",     limit: 8
-    t.integer "water_area",    limit: 8
-    t.string  "time_zone",     limit: 30
-  end
+  
 
 end
